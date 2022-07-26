c3f553f1c4ee517ad70db4477659f9d60688561d
David Hevesi <davidhevesi@Davids-MacBook-Pro.local>
2022-07-26 11:59:08 -0400

    add custom colors and breakpoints

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="bg-blue-500">hello</div>;
+  return <div className="bg-dark-blue-gray">hello</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      mobile: "375px",
+      tablet: "768px",
+      desktop: "1440px",
+    },
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      "frost-bite": "hsl(322, 87%, 55%)",
+      "outrageous-orange": "hsl(13, 100%, 64%)",
+      "dark-blue-gray": "hsl(234, 25%, 52%)",
+      "yankee-blue": "hsl(233, 54%, 16%)",
+      "roman-silver": "hsl(232, 10%, 56%)",
+      "sasquatch-socks": "hsl(341, 92%, 62%)",
+      "carnation-pink": "hsl(341°, 100%, 83%)",
+      "ultramarine-blue": "hsl(237, 100%, 64%)",
+    },
     extend: {},
   },
   plugins: [],
